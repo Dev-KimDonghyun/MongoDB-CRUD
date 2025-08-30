a679b51cf8dcd103226117497b2a51a379c76e3d
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-30 11:26:02 +0900

    feat: Add Create User Controller

---
+++ src/controllers/control.create.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const User = require("../models/user.model.js");
+const router = express.Router();
+
+router.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ success: true, data: user });
+  } catch (err) {
+    res.status(400).json({ success: false, message: "Invalid request" });
+  }
+});
