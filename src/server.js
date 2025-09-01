831d51417ea11002edfb0813a35c350768b06b8c
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 20:15:58 +0900

    feat: Connect CRUD Controllers in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
 
+const connectDatabase = require("./config/database");
+
+const createControl = require("./controllers/control.create");
+const readControl = require("./controllers/control.read");
+const updateControl = require("./controllers/control.update");
+const deleteControl = require("./controllers/control.delete");
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+connectDatabase();
+
+app.use("/api", createControl);
+app.use("/api", readControl);
+app.use("/api", updateControl);
+app.use("/api", deleteControl);
 
 app.get("/", (req, res) => {
   res.send("Hello!");
