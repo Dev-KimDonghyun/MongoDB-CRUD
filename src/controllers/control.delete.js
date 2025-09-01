be6a0906749c52272c60dae0e268be37511191cc
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 19:29:08 +0900

    feat: Add Delete User by Id Controller

---
+++ src/controllers/control.delete.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const User = require("../models/user.model.js");
+const router = express.Router();
+
+router.delete("/users/:id", async (req, res) => {
+  const _ID = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(_ID);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    res.json({ success: true, data: user });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
