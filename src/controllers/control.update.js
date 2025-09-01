81e9f32846d932ab3d9529046581ad481ed09b02
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 19:53:42 +0900

    refactor: Replace PUT with PATCH for User Update

--- src/controllers/control.update.js
+++ src/controllers/control.update.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("../models/user.model.js");
 const router = express.Router();
 
-router.put("/users/:id", async (req, res) => {
+router.patch("/users/:id", async (req, res) => {
   const _ID = req.params.id;
   const { nickName, comment } = req.body;
   try {
