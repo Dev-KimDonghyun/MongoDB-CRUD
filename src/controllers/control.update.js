98d7ab3b6cac400a22576e0184140a63a6629122
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 19:20:17 +0900

    feat: Add Update User by Id Controller

---
+++ src/controllers/control.update.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const User = require("../models/user.model.js");
+const router = express.Router();
+
+router.put("/users/:id", async (req, res) => {
+  const _ID = req.params.id;
+  const { nickName, comment } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(
+      _ID,
+      { nickName, comment },
+      { new: true }
+    );
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    res.json({ success: true, data: user });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
